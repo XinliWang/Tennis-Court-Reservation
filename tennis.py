44c1ad80568ddf3cf59a24c7241e22492f23f896
Hongbing Cai <hongbing.cai@utexas.edu>
2018-07-19 12:06:17 -0400

    fix limited resource problem

--- tennis.py
+++ tennis.py
@@ -60,6 +60,8 @@ def reserve(tomorrow, reserveid, starttime, endtime):
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_argument("--incognito")
 	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
 	# open chrome
 	driver = webdriver.Chrome(chrome_options=chrome_options)
 	driver.implicitly_wait(15)
