6e555625bee18c6db52fe793e97aafc819b64be4
Hongbing Cai <hongbing.cai@utexas.edu>
2018-07-19 12:01:06 -0400

    add chrome installer for ubuntu

---
+++ ubuntuchrome.sh
@@ -0,0 +1,17 @@
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+
+sudo apt-get install xvfb
+
+sudo apt-get install unzip
+
+wget -N http://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
